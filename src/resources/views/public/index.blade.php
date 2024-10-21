2fbad956ca28d6954aa3991f016daa2eb7dec571
Samuel De Backer <sdebacker@gmail.com>
2024-10-21 10:39:22 +0200

    Update index.blade.php

--- src/resources/views/public/index.blade.php
+++ src/resources/views/public/index.blade.php
@@ -3,14 +3,12 @@
 @section('bodyClass', 'body-things body-things-index body-page body-page-' . $page->id)
 
 @section('page')
+    @include('things::public._itemlist-json-ld', ['items' => $models])
     <div class="page-body">
         <div class="page-body-container">
-            <div class="rich-content">{!! $page->present()->body !!}</div>
-
+            @include('pages::public._main-content', ['page' => $page])
             @include('files::public._document-list', ['model' => $page])
             @include('files::public._image-list', ['model' => $page])
-
-            @include('things::public._itemlist-json-ld', ['items' => $models])
 
             @includeWhen($models->count() > 0, 'things::public._list', ['items' => $models])
         </div>
