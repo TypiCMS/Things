4d873cd9ffad5f15a3ffd4f40d635e89fcbb397f
Samuel De Backer <sdebacker@gmail.com>
2025-06-09 18:27:12 +0200

    @property fixed

--- src/Models/Thing.php
+++ src/Models/Thing.php
@@ -16,11 +16,11 @@ use TypiCMS\Modules\Things\Presenters\ModulePresenter;
 /**
  * @property int $id
  * @property int|null $image_id
- * @property array<array-key, mixed> $status
- * @property array<array-key, mixed> $title
- * @property array<array-key, mixed> $slug
- * @property array<array-key, mixed> $summary
- * @property array<array-key, mixed> $body
+ * @property string $status
+ * @property string $title
+ * @property string $slug
+ * @property string $summary
+ * @property string $body
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
