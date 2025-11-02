6517eacd2e8169603e5473c329d5be1ae7ccac54
Samuel De Backer <sdebacker@gmail.com>
2025-11-02 19:39:32 +0100

    @context escaped in json-ld files

--- src/resources/views/public/_json-ld.blade.php
+++ src/resources/views/public/_json-ld.blade.php
@@ -1,7 +1,7 @@
 {{--
     <script type="application/ld+json">
     {
-    "@context": "http://schema.org",
+    "@@context": "http://schema.org",
     "@type": "",
     "name": "{{ $thing->title }}",
     "description": "{{ $thing->summary !== '' ? $thing->summary : strip_tags($thing->body) }}",
